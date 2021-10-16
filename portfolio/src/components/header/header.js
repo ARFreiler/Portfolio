e4242a8a82b4ff72ee1a0da8158259f9547880f8
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-10-16 13:06:11 -0400

    Created initial header structure.

---
+++ portfolio/src/components/header/header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="into">
+                <h1>Ashleigh Freiler</h1>
+                <p>an evolving human in all things great and small</p>
+            </div>
+            <nav>
+                <a className="nav-links"></a>
+                <ul className="nav-list">
+                    <li className="nav-item">Home</li>
+                    <li className="nav-item">My Work</li>
+                    <li className="nav-item">Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
