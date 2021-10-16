2ac83dc5a9c15c8216e2bd1430964dc149fd5b54
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-10-16 15:40:40 -0400

    Added styling to the haeder component.

--- portfolio/src/components/header/header.js
+++ portfolio/src/components/header/header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import '../header/header.css'
 
 const Header = () => {
     return (
         <header className="header">
             <div className="intro">
                 <h1 id="intro-name">Ashleigh Freiler</h1>
-                <p id="intro-text">an evolving human in all things great and small</p>
+                <p id="intro-text"><span className="intro-span">&#60;</span> an evolving human in all things great and small <span className="intro-span">&#47;&#62;</span></p>
             </div>
             <div className="nav">
                 <div className="nav-links">
@@ -16,7 +17,7 @@ const Header = () => {
                     <button className="nav-link">Contact</button>
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
 
