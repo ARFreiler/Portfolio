48fd2433cfacd07a145a71c5c753a67f9bcb30b2
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-10-25 21:02:33 -0400

    Reworking nav.

--- portfolio/src/scenes/about-scene/about.js
+++ portfolio/src/scenes/about-scene/about.js
@@ -11,9 +11,11 @@ const About = () => {
         <div className="about-container">
             <Header />
             <div className="about-content">
-                <h1 className="about-title">ABOUT ME ELEMENT TITLE</h1>
-                <p className="about-text-one">Former anti-tech. Dog mom. Yoga practicioner. Nature enthusiast. Plant parent. Eternal student.</p>
-                <p className="about-text-two">Located in Philadelphia, Pennsylvania. Urban dweller, rural explorer. Living each day, faithful pitbull by my side. Growing as a person, evolving as a human.</p>
+                <div className="about-content-text">
+                    <h1 className="about-title">ABOUT ME ELEMENT TITLE</h1>
+                    <p className="about-text-one">Former anti-tech. Dog mom. Yoga practicioner. Nature enthusiast. Plant parent. Eternal student.</p>
+                    <p className="about-text-two">Located in Philadelphia, Pennsylvania. Urban dweller, rural explorer. Living each day, faithful pitbull by my side. Growing as a person, evolving as a human.</p>
+                </div>
             </div>
             <Footer />
         </div>
