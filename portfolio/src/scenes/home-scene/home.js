c92f2e6fb556a54bc9498714c74686cf06174412
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-10-26 09:30:34 -0400

    Added google fonts and changed text accordingly.

--- portfolio/src/scenes/home-scene/home.js
+++ portfolio/src/scenes/home-scene/home.js
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <div className="home-container">
             <Header />
-            <Content />
+            <div className="content"><Content /></div>
             <Footer />
         </div>
     )
